3c0ae1b4aa0d0b97dcd94c5ac96338a36681ba6d
nishi <nishi@9ef645bc-c79c-44e9-9d58-e04c46e47e10>
2025-08-14 15:25:48 +0000

    regenerate
    git-svn-id: http://svn2.nishi.boats/svn/pvi/trunk@57 9ef645bc-c79c-44e9-9d58-e04c46e47e10

--- mk/depend.mk
+++ mk/depend.mk
@@ -3,3 +3,5 @@ src/main.o: src/main.c include/common.h include/machdep.h config.h
 src/arg.o: src/arg.c include/common.h include/machdep.h config.h
 src/array.o: src/array.c include/common.h include/machdep.h config.h
 src/util.o: src/util.c include/common.h include/machdep.h config.h
+src/d_windows.o: src/d_windows.c include/common.h include/machdep.h config.h
+src/d_termcap.o: src/d_termcap.c include/common.h include/machdep.h config.h
